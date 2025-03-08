675e883a0af15721c18b8f9d63ab55adc1bfe55c
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2025-03-08 16:28:53 +0500

    Update powAbbrev.js

--- powAbbrev.js
+++ powAbbrev.js
@@ -28,7 +28,7 @@ function t2abbrev(n,tier2)
 	tier2 = Math.floor(tier2)
 	if (tier2 == 1)
 	{
-		return ((n == 1) ? "" : (abbreviations[1][n%10] + abbreviations[2][Math.floor(n/10)%10] + abbreviations[3][Math.floor(n/100)])) + ((n == 1000**Math.floor(Math.log10(n)/3)/10) ? abbreviations[4][tier2] : abbreviations2[tier2]) + ((tier2 < 1) ? abbrev(Math.floor(n*1000)%1000) : t2abbrev(Math.floor(n*1000)%1000,tier2-1)
+		return ((n == 1) ? "" : (abbreviations[1][n%10] + abbreviations[2][Math.floor(n/10)%10] + abbreviations[3][Math.floor(n/100)])) + ((n == 1000**Math.floor(Math.log10(n)/3)/10) ? abbreviations[4][tier2] : abbreviations2[tier2]) + ((tier2 < 1) ? abbrev(Math.floor(n*1000)%1000) : t2abbrev(Math.floor(n*1000)%1000,tier2-1))
 	}
 	else
 	{
