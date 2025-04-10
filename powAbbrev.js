02a9233712b49fc1f5a27686e51de69e25322c72
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-04-10 17:35:03 +0500

    Update powAbbrev.js

--- powAbbrev.js
+++ powAbbrev.js
@@ -3,52 +3,45 @@ let abbreviations = [
 	["", "U", "D", "T", "Qd", "Qn", "Sx", "Sp", "Ot", "No"],
 	["", "De", "Vg", "Tg", "Qdg", "Qng", "Sxg", "Spg", "Otg", "Nog"],
 	["", "Ce", "Dc", "Tc", "Qdc", "Qnc", "Sxc", "Spc", "Otc", "Noc"],
-	["", "Mil", "Mic", "Nan", "Pic", "Fmt", "Att", "Zep", "Yoc", "Xon", "Vec", "Mec", "Duec", "Trec", "Ttrec", "Pntec", "Hxec", "Hpec", "Ocec", "Enec", "Ico", "MeIco", "DueIco", "TreIco", "TtrIco", "PntIco", "HxeIco", "HpeIco", "OceIco", "EneIco", "Tic", "MeTic", "DueTic", "TreTic", "TtrTic", "PntTic", "HxeTic", "HpeTic", "OceTic", "EneTic", "Ttc", "MeTtc", "DueTtc", "TreTtc", "TtrTtc", "PntTtc", "HxeTtc", "HpeTtc", "OceTtc", "EneTtc", "Pnc", "MePnc", "DuePnc", "TrePnc", "TtrPnc", "PntPnc", "HxePnc", "HpePnc", "OcePnc", "EnePnc", "Hxc", "MeHxc", "DueHxc", "TreHxc", "TtrHxc", "PntHxc", "HxeHxc", "HpeHxc", "OceHxc", "EneHxc", "Hpc", "MeHpc", "DueHpc", "TreHpc", "TtrHpc", "PntHpc", "HxeHpc", "HpeHpc", "OceHpc", "EneHpc", "Occ", "MeOcc", "DueOcc", "TreOcc", "TtrOcc", "PntOcc", "HxeOcc", "HpeOcc", "OceOcc", "EneOcc", "Enc", "MeEnc", "DueEnc", "TreEnc", "TtrEnc", "PntEnc", "HxeEnc", "HpeEnc", "OceEnc", "EneEnc"],
+	["", "Mil", "Mic", "Nan", "Pic", "Fmt", "Att", "Zep", "Yoc", "Xon", "Vec", "Mec", "Duec", "Trec", "Ttrec", "Pntec", "Hxec", "Hpec", "Ocec", "Enec", "Ico", "MeIco", "DueIco", "TreIco", "TtrIco", "PntIco", "HxeIco", "HpeIco", "OceIco", "EneIco", "Tic", "MeTic", "DueTic", "TreTic", "TtrTic", "PntTic", "HxeTic", "HpeTic", "OceTic", "EneTic", "Ttc", "MeTtc", "DueTtc", "TreTtc", "TtrTtc", "PntTtc", "HxeTtc", "HpeTtc", "OceTtc", "EneTtc", "Pnc", "MePnc", "DuePnc", "TrePnc", "TtrPnc", "PntPnc", "HxePnc", "HpePnc", "OcePnc", "EnePnc", "Hxc", "MeHxc", "DueHxc", "TreHxc", "TtrHxc", "PntHxc", "HxeHxc", "HpeHxc", "OceHxc", "EneHxc", "Hpc", "MeHpc", "DueHpc", "TreHpc", "TtrHpc", "PntHpc", "HxeHpc", "HpeHpc", "OceHpc", "EneHpc", "Occ", "MeOcc", "DueOcc", "TreOcc", "TtrOcc", "PntOcc", "HxeOcc", "HpeOcc", "OceOcc", "EneOcc", "Enc", "MeEnc", "DueEnc", "TreEnc", "TtrEnc", "PntEnc", "HxeEnc", "HpeEnc", "OceEnc", "EneEnc", "Hct", "MeHct", "DueHct"],
 ]
-let abbreviations2 = ["", "Mil-", "Mic-", "Nan-", "Pic-", "Fmt-", "Att-", "Zep-", "Yoc-", "Xon-", "Vec-", "Mec-", "Duec-", "Trec-", "Ttrec-", "Pntec-", "Hxec-", "Hpec-", "Ocec-", "Enec-", "Ico-", "MeIco-", "DueIco-", "TreIco-", "TtrIco-", "PntIco-", "HxeIco-", "HpeIco-", "OceIco-", "EneIco-", "Tic-", "MeTic-", "DueTic-", "TreTic-", "TtrTic-", "PntTic-", "HxeTic-", "HpeTic-", "OceTic-", "EneTic-", "Ttc-", "MeTtc-", "DueTtc-", "TreTtc-", "TtrTtc-", "PntTtc-", "HxeTtc-", "HpeTtc-", "OceTtc-", "EneTtc-", "Pnc-", "MePnc-", "DuePnc-", "TrePnc-", "TtrPnc-", "PntPnc-", "HxePnc-", "HpePnc-", "OcePnc-", "EnePnc-", "Hxc-", "MeHxc-", "DueHxc-", "TreHxc-", "TtrHxc-", "PntHxc-", "HxeHxc-", "HpeHxc-", "OceHxc-", "EneHxc-", "Hpc-", "MeHpc-", "DueHpc-", "TreHpc-", "TtrHpc-", "PntHpc-", "HxeHpc-", "HpeHpc-", "OceHpc-", "EneHpc-", "Occ-", "MeOcc-", "DueOcc-", "TreOcc-", "TtrOcc-", "PntOcc-", "HxeOcc-", "HpeOcc-", "OceOcc-", "EneOcc-", "Enc-", "MeEnc-", "DueEnc-", "TreEnc-", "TtrEnc-", "PntEnc-", "HxeEnc-", "HpeEnc-", "OceEnc-", "EneEnc-"]
-function abbrev(n)
+function t2abbrev(n,layer)
 {
 	n = Math.floor(n)
-	if (n >= 100)
+	layer = Math.floor(layer)
+	if (layer == 1)
 	{
-		return abbreviations[1][n%10] + abbreviations[2][Math.floor(n/10)%10] + abbreviations[3][Math.floor(n/100)]
+		return ((n == 1n) ? "" : (abbreviations[1][BigInt(n%10)] + abbreviations[2][BigInt(Math.floor(n/10)%10)] + abbreviations[3][BigInt(Math.floor(n/100))])) + abbreviations[4][BigInt(layer)]
+	}
+	else if (n >= 100)
+	{
+		return abbreviations[1][BigInt(n%10)] + abbreviations[2][BigInt(Math.floor(n/10)%10)] + abbreviations[3][BigInt(Math.floor(n/100))]
 	}
 	else if (n >= 10)
 	{
-		return abbreviations[1][n%10] + abbreviations[2][Math.floor(n/10)]
+		return abbreviations[1][BigInt(n%10)] + abbreviations[2][BigInt(Math.floor(n/10))]
 	}
 	else if (n >= 0)
 	{
-		return abbreviations[0][n]
-	}
-}
-function t2abbrev(n,tier2)
-{
-	n = Math.floor(n)
-	tier2 = Math.floor(tier2)
-	if (tier2 == 1)
-	{
-		return ((n == 1) ? "" : (abbreviations[1][n%10] + abbreviations[2][Math.floor(n/10)%10] + abbreviations[3][Math.floor(n/100)])) + ((n == 1000**Math.floor(Math.log10(n)/3)/10) ? abbreviations[4][tier2] : abbreviations2[tier2]) + ((tier2 < 1) ? abbrev(Math.floor(n*1000)%1000) : t2abbrev(Math.floor(n*1000)%1000,tier2-1))
-	}
-	else
-	{
-		return abbrev(n)
+		return abbreviations[0][BigInt(n)]
 	}
 }
 class powAbbrev
 {
 	constructor(base,pow)
 	{
-		this.result = 0;
+		this.result = 0n;
 		this.base = base;
 		this.pow = pow;
-		if (Math.log10(base)*pow >= 3)
+		let n = Math.log10(base)*pow;
+		let m = Math.log10(n)/3;
+		if (n >= 3n)
 		{
-			this.result = ((Math.log10(base)*pow >= 1_000_000) ? "" : Math.pow(10,(Math.log10(this.base)*this.pow)%3).toFixed(3)) + t2abbrev((Math.log10(this.base)*this.pow)/1000**Math.floor(Math.log10(Math.log10(this.base)*this.pow)/3),1000**Math.floor(Math.log10(Math.log10(this.base)*this.pow)/3))
+			this.result = ((n >= 1_000_000n) ? "" : Math.pow(10,n%3).toFixed(3)) + ((n == Math.floor(n/1000**Math.floor(m))*(1000**Math.floor(m))) ? t2abbrev(n/1000**Math.floor(m),m) : (t2abbrev(n/1000**Math.floor(m),m)+"-"+t2abbrev((n/1000**Math.floor(m-1))%1000,m-1)))
 		}
 		else
 		{
-			this.result = Math.pow(10,(Math.log10(this.base)*this.pow)%3).toFixed(3)
+			this.result = Math.pow(10,n).toFixed(3)
 		}
 		return this.result
 	}
